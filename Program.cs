94e9b553397c18935140256df6b196662017d663
Lailton Soares Pinho <69491613+LailtonSoares@users.noreply.github.com>
2023-03-30 23:02:04 -0300

    Enviando primeira parte do projeto

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using trilha_net_poo_desafio.Models;
+
+Console.WriteLine("Smartphone Nokia: ");
+Nokia nokia = new Nokia("123", "1198797979", "Modelo 1", 32);
+nokia.InstalarAplicativo("Whatassp");
+nokia.Ligar();
+
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Iphone iphone = new Iphone("123", "198799444", "Modelo 2", 16);
+iphone.InstalarAplicativo("Telegram");
+iphone.ReceberLigacao();
